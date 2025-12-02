a3e19c8e5ea60f5a1b0ecf9bcf91a17c22c9fef0
Michael Bestas <mkbestas@lineageos.org>
2025-12-02 15:25:15 +0200

    comet: Rework sepolicy
    Change-Id: Idb0636bce2392beb720e420055a7bcb838725a18

--- comet/BoardConfig.mk
+++ comet/BoardConfig.mk
@@ -10,7 +10,6 @@ TARGET_BOOTLOADER_BOARD_NAME := comet
 TARGET_SCREEN_DENSITY := 390
 
 include device/google/zumapro/BoardConfig-common.mk
-include device/google/comet/sepolicy/comet-sepolicy.mk
 include device/google/comet/wifi/BoardConfig-wifi.mk
 
 # Kernel modules
@@ -18,5 +17,11 @@ BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/reco
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_kernel_boot))
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+
+# SEPolicy
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/google/comet/sepolicy/vendor \
+    hardware/google/pixel-sepolicy/vibrator/common \
+    hardware/google/pixel-sepolicy/vibrator/cs40l26
 
 include $(VENDOR_PATH)/BoardConfigVendor.mk
